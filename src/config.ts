613d1b2d7147045bf16770c6affd57d9fef04f7e
cwhinfrey <cwhinfrey@gmail.com>
2021-07-12 09:11:28 -0700

    point to mainnnet

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const hopUrl = 'https://kovan.hop.exchange/'
+export const hopUrl = 'https://app.hop.exchange/'
 export const discordUrl = 'https://discord.gg/2BxafMKP'
 export const docsUrl = 'https://docs.hop.exchange/'
 export const githubUrl = 'https://github.com/hop-protocol'
