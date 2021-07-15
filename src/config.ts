f045879c58272ea081cc458a2b699e72a5bc5c1b
Shane Fontaine <shane6fontaine@gmail.com>
2021-07-15 09:14:57 -0700

    Update discord link

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const hopUrl = 'https://app.hop.exchange/'
-export const discordUrl = 'https://discord.gg/2BxafMKP'
+export const discordUrl = 'https://discord.gg/PwCF88emV4'
 export const docsUrl = 'https://docs.hop.exchange/'
 export const githubUrl = 'https://github.com/hop-protocol'
 export const twitterUrl = 'https://twitter.com/HopProtocol'
