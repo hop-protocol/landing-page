0c9667bd60f2eded6f50075e813a78b3889c8621
cwhinfrey <cwhinfrey@gmail.com>
2021-07-30 15:21:16 -0700

    Update footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,27 @@
 import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import background from '../assets/landing-page-bg.svg';
 import logo from '../assets/hop-logo.svg';
 import discord from '../assets/discord.svg';
 import github from '../assets/github.svg';
-import docs from '../assets/docs.svg';
+import medium from '../assets/medium.svg';
 import twitter from '../assets/twitter.svg';
 import useHop from '../assets/use-hop.svg';
 import Box from '@material-ui/core/Box'
-import Link from '@material-ui/core/Link'
+import Typography from '@material-ui/core/Typography'
+import MuiLink from '@material-ui/core/Link'
 import {
   hopUrl,
   discordUrl,
   docsUrl,
+  faqUrl,
   githubUrl,
+  mediumUrl,
   twitterUrl
 } from '../config'
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
   background: {
     zIndex: -1,
     position: 'fixed',
@@ -35,16 +39,22 @@ const useStyles = makeStyles(() => ({
   },
   link: {
     margin: '1.8rem',
-    width: '3.5rem',
+    width: '3.0rem',
     opacity: '35%',
     '&:hover': {
       opacity: '70%'
     }
   },
   links: {
-    marginLeft: '0.4rem',
     position: 'absolute',
-    bottom: '2.4rem'
+    bottom: '1.4rem'
+  },
+  footerLink: {
+    textDecoration: 'none',
+    margin: `0 ${theme.padding.extraLight}`,
+    '&:hover': {
+      textDecoration: 'inherit'
+    }
   },
   useHop: {
     height: '5.0rem'
@@ -76,48 +86,85 @@ const Home: FC<Props> = () => {
         className={styles.container}
       >
         <img src={logo} className={styles.logo} alt="logo" />
-        <Link
+        <MuiLink
           href={hopUrl}
           target="_blank"
           rel="noopener noreferrer"
           className={styles.appButton}
         >
           <img src={useHop} className={styles.useHop} alt="logo" />
-        </Link>
+        </MuiLink>
         <Box
           display="flex"
-          flexDirection="row"
+          flexDirection="column"
           alignItems="center"
           className={styles.links}
         >
-          <Link
-            href={discordUrl}
-            target="_blank"
-            rel="noopener noreferrer"
+          <Box
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
           >
-            <img src={discord} className={styles.link} alt="logo" />
-          </Link>
-          <Link
-            href={twitterUrl}
-            target="_blank"
-            rel="noopener noreferrer"
+            <MuiLink
+              href={discordUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={discord} className={styles.link} alt="logo" />
+            </MuiLink>
+            <MuiLink
+              href={twitterUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={twitter} className={styles.link} alt="logo" />
+            </MuiLink>
+            <MuiLink
+              href={githubUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={github} className={styles.link} alt="logo" />
+            </MuiLink>
+            <MuiLink
+              href={mediumUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={medium} className={styles.link} alt="logo" />
+            </MuiLink>
+          </Box>
+          <Box
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
           >
-            <img src={twitter} className={styles.link} alt="logo" />
-          </Link>
-          <Link
-            href={githubUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={github} className={styles.link} alt="logo" />
-          </Link>
-          <Link
-            href={docsUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={docs} className={styles.link} alt="logo" />
-          </Link>
+            <MuiLink
+              href={faqUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.footerLink}
+            >
+              <Typography variant="subtitle2" color="primary">
+                FAQ
+              </Typography>
+            </MuiLink>
+            <MuiLink
+              href={faqUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.footerLink}
+            >
+              <Typography variant="subtitle2" color="primary">
+                Docs
+              </Typography>
+            </MuiLink>
+            <Link to="/careers" className={styles.footerLink}>
+              <Typography variant="subtitle2" color="primary">
+                Careers
+              </Typography>
+            </Link>
+          </Box>
         </Box>
       </Box>
     </>
